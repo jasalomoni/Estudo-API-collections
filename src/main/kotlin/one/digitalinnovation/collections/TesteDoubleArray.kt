3f597b73485beb0df6b05874c30b3a59257c4604
José Augusto Salomoni <zeca.salomoni@gmail.com>
2021-02-23 16:45:14 -0300

    crinado teste de array double

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteDoubleArray.kt
@@ -0,0 +1,23 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val salarios = DoubleArray(3)
+
+    salarios[0] = 1000.0
+    salarios[1] = 4000.0
+    salarios[2] = 500.0
+
+    salarios.forEach { println(it) }
+
+    println("====================")
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * 1.1
+    }
+    salarios.forEach { println(it) }
+
+    println("====================")
+    val salarios2 = doubleArrayOf(1500.0, 3600.0, 2100.0)
+    salarios2.sort()
+    salarios2.forEach { println(it) }
+
+}
