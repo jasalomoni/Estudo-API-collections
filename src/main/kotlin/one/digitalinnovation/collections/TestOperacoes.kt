2945856d6239c7546aeabd20cd8cab1ea86eeb78
José Augusto Salomoni <zeca.salomoni@gmail.com>
2021-02-23 17:37:44 -0300

    crinado teste de array com operações (count, find e any)

--- src/main/kotlin/one/digitalinnovation/collections/TestOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestOperacoes.kt
@@ -12,4 +12,14 @@ fun main() {
 
     salarioMaior4200.forEach { println(it) }
 
+    println("=============================")
+    println(salarios.count { it in 2000.0..6000.0 })
+
+    println("=============================")
+    println(salarios.find { it == 4100.0 })
+    println(salarios.find { it == 100.0 })
+
+    println("=============================")
+    println(salarios.any { it == 4100.0 })
+    println(salarios.any { it == 4200.0 })
 }
