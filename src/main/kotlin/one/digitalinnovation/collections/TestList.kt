4698a8fd0478e03b5807887b9ea3fbd16dd3b026
José Augusto Salomoni <zeca.salomoni@gmail.com>
2021-02-24 11:09:56 -0300

    alterado o TesteList e incluido o arquivo TestSet com comandos setOf, union, subtract e intersect

--- src/main/kotlin/one/digitalinnovation/collections/TestList.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestList.kt
@@ -21,14 +21,3 @@ fun main() {
         .groupBy { it.tipoContrato }
         .forEach { println(it) }
 }
-
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContrato: String
-) {
-    override fun toString(): String =
-        """
-            Nome: $nome    Salário: $salario
-        """.trimIndent()
-}
